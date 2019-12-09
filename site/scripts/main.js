1981b9f7ccd4bfe8cb115ecce8ad26b3e448d6d7
Paweł Chłąd <pchlad5@gmail.com>
2019-12-09 16:16:36 +0100

    - Added new event listeners to canvas (now user must click to draw) - Added .gitignore

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -2,16 +2,27 @@
 window.onload = function()
 {
 	var module = new DrawingModule();
+	var drawing = false;
 
 	function draw(env)
 	{
-		var curr_point = new Point(env.ClientX, env.ClientY);
-		curr_point.x -= module.rect.left;
-		curr_point.y -= module.rect.top;
-		module.place_pixel(curr_point);
+		if(drawing == true)
+		{
+
+			var curr_point = new Point(env.clientX, env.clientY);
+			curr_point.x -= module.rect.left;
+			curr_point.y -= module.rect.top;
+			module.place_pixel(curr_point);
+
+		}
+		
 	}
 
 	module.canvas.onmousemove = draw;
+	module.canvas.onmouseup = function() {drawing = false;};
+	module.canvas.onmouseleave = function() {drawing = false;};
+	module.canvas.onmousedown = function() {drawing = true;};
+
 }
 
 
