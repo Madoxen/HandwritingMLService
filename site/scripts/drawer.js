061c7470b1834315a3e7ee22204b857e6d212ba9
Paweł Chłąd <pchlad5@gmail.com>
2019-12-09 17:10:03 +0100

    Finished frontend work

--- site/scripts/drawer.js
+++ site/scripts/drawer.js
@@ -6,13 +6,26 @@ class DrawingModule
 		this.context = this.canvas.getContext("2d");
 		this.rect = this.canvas.getBoundingClientRect();
 		this.scale = new Point(this.canvas.width/this.rect.width, this.canvas.height / this.rect.height);	
+		this.last_point = new Point();
 	}
 
 	
-	place_pixel(point)
+	draw(point)
 	{
-		this.context.fillStyle = "#000000";
-    		this.context.fillRect (point.x, point.y, 1, 1);	
+		this.context.beginPath();
+  		this.context.strokeStyle = 'black';
+  		this.context.lineWidth = 3;
+  		this.context.moveTo(this.last_point.x, this.last_point.y);
+  		this.context.lineTo(point.x, point.y);
+  		this.context.stroke();
+  		this.context.closePath();
+		this.last_point = point;
+	}
+
+
+	clear()
+	{
+		this.context.clearRect(0,0, this.canvas.width, this.canvas.height);	
 	}
 }
 
--- site/scripts/main.js
+++ site/scripts/main.js
@@ -2,26 +2,42 @@
 window.onload = function()
 {
 	var module = new DrawingModule();
+	var clear_button = document.getElementById("clear_button")
 	var drawing = false;
 
 	function draw(env)
 	{
-		if(drawing == true)
+		if(drawing === true)
 		{
 
 			var curr_point = new Point(env.clientX, env.clientY);
 			curr_point.x -= module.rect.left;
 			curr_point.y -= module.rect.top;
-			module.place_pixel(curr_point);
+			module.draw(curr_point);
 
 		}
 		
 	}
 
+	function stopDrawing()
+	{
+		drawing = false;
+		module.last_point = new Point();
+	}
+
+	function startDrawing()
+	{
+		drawing = true;
+	}
+
+
+
+	//Add canvas event handlers
 	module.canvas.onmousemove = draw;
-	module.canvas.onmouseup = function() {drawing = false;};
-	module.canvas.onmouseleave = function() {drawing = false;};
-	module.canvas.onmousedown = function() {drawing = true;};
+	module.canvas.onmouseup = stopDrawing;
+	module.canvas.onmouseleave = stopDrawing;
+	module.canvas.onmousedown = startDrawing;
+	clear_button.onclick = function() {module.clear()};
 
 }
 
