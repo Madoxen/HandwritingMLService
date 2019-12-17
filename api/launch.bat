f61eca6d381485458bfc48a50a5729961485e9ac
Paweł Chłąd <pchlad5@gmail.com>
2019-12-17 03:21:20 +0100

    Final

---
+++ api/launch.bat
@@ -0,0 +1,37 @@
+@ECHO OFF
+
+
+ECHO Welcome to simple classifier menu
+ECHO Choose your option
+ECHO 1. Starts the server
+ECHO 2. Backups model
+ECHO 3. Exits the program
+
+SET /P choice="Choice>>"
+
+:MENU
+
+IF %choice% EQU 1 (
+GOTO ONE
+)
+IF %choice% EQU 2 (
+GOTO TWO
+)
+IF %choice% EQU 3 (
+GOTO THREE
+)
+
+:ONE
+START server.exe
+GOTO MENU
+
+
+:TWO
+XCOPY server.exe ../backup
+XCOPY network.json ../backup
+XCOPY ../src ../backup
+GOTO MENU
+
+
+:THREE
+
